96f21374c37943807dfdf2f8ad220eb34bfc65f7
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2022-10-25 20:03:56 +0530

    path parameters with types

--- Path_Paramaters/main.py
+++ Path_Paramaters/main.py
@@ -1,8 +1,21 @@
+# from fastapi import FastAPI
+
+# app = FastAPI
+
+
+# @app.get("/items/{item_id")
+# async def read_items(item_id):
+#     return {"item_id": item_id}
+
+
+"""
+path parameters with types
+"""
 from fastapi import FastAPI
 
 app = FastAPI
 
 
 @app.get("/items/{item_id")
-async def read_items(item_id):
+async def read_items(item_id: int):
     return {"item_id": item_id}
