830267bc065af8685ba4a6f0b829165f823d33fa
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2022-10-25 20:09:34 +0530

    path parameters order matters

--- Path_Paramaters/main.py
+++ Path_Paramaters/main.py
@@ -1,6 +1,6 @@
 # from fastapi import FastAPI
 
-# app = FastAPI
+# app = FastAPI()
 
 
 # @app.get("/items/{item_id")
@@ -11,11 +11,29 @@
 """
 path parameters with types
 """
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+
+# @app.get("/items/{item_id")
+# async def read_items(item_id: int):
+#     return {"item_id": item_id}
+
+
+"""
+order matters
+"""
 from fastapi import FastAPI
 
-app = FastAPI
+app = FastAPI()
 
 
-@app.get("/items/{item_id")
-async def read_items(item_id: int):
-    return {"item_id": item_id}
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
